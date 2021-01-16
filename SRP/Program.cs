dc72016537e65d60003ed7970954484bdfb3bbd6
Ramzan <muhammad.ramzan@brainstation23.com>
2021-01-16 15:29:36 +0600

    Initial commit

--- SRP/Program.cs
+++ SRP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mail;
 
 namespace SRP
 {
@@ -7,6 +8,25 @@ namespace SRP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class InvitationService
+    {
+        public void SendInvite(string email, string firstName, string lastName)
+        {
+            if (String.IsNullOrWhiteSpace(firstName) || String.IsNullOrWhiteSpace(lastName))
+            {
+                throw new Exception("Name is not valid!");
+            }
+
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                throw new Exception("Email is not valid!");
+            }
+
+            SmtpClient client = new SmtpClient();
+            client.Send(new MailMessage("mysite@nowhere.com", email) { Subject = "Please join me at my party!" });
         }
     }
 }
