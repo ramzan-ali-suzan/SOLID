8b782324dc8ee40ea1627db4fc0ae2beb41eca5a
Ramzan <muhammad.ramzan@brainstation23.com>
2021-01-16 16:02:05 +0600

    Some re-factoring done

---
+++ SRP/InvitationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SRP
+{
+    public class InvitationService
+    {
+        public void SendInvite(string email, string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            {
+                Console.WriteLine("Name is not valid!");
+            }
+
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                Console.WriteLine("Email is not valid!");
+            }
+
+            Console.WriteLine($"Sending invitation to {firstName} {lastName}, at email {email}...");
+            Console.ReadKey();
+        }
+    }
+}
--- SRP/Program.cs
+++ SRP/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Mail;
 
 namespace SRP
 {
@@ -7,26 +6,8 @@ namespace SRP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
-    public class InvitationService
-    {
-        public void SendInvite(string email, string firstName, string lastName)
-        {
-            if (String.IsNullOrWhiteSpace(firstName) || String.IsNullOrWhiteSpace(lastName))
-            {
-                throw new Exception("Name is not valid!");
-            }
-
-            if (!email.Contains("@") || !email.Contains("."))
-            {
-                throw new Exception("Email is not valid!");
-            }
-
-            SmtpClient client = new SmtpClient();
-            client.Send(new MailMessage("mysite@nowhere.com", email) { Subject = "Please join me at my party!" });
+            var invitationService = new InvitationService();
+            invitationService.SendInvite("hasan@mail.com", "Hasan", "Imam");
         }
     }
 }
